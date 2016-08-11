27b1272b001024e3b41249bdd077fd3b39bd0b70
Joshua <joshua@eduku.org>
2016-08-11 10:01:59 +0000

    added a new user with associated postSchema and tested

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,16 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
 
-//New Mongoose Schema with the users' email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String
-});
-
-//Mongoose model
-
-var User = mongoose.model("User", userSchema);
-
 //POST - title, content
 
 var postSchema = new mongoose.Schema({
@@ -20,29 +10,49 @@ var postSchema = new mongoose.Schema({
 
 var Post = mongoose.model("Post", postSchema);
 
-// var newUser = new User({
-//     email: "joshua@myemail.com",
-//     name: "Joshua Maddox"
-// });
-
-// newUser.save(function(err, user){
-//   if(err){
-//       console.log(err);
-//   } else {
-//       console.log(user);
-//   }
-// });
-
-var newPost = new Post({
-    title: "Making Associative Data with MongoDB",
-    content: "Thundercats ramps etsy try-hard twee +1 williamsburg, farm-to-table readymade direct trade man bun kombucha fashion axe. Narwhal kitsch trust fund, cliche yuccie green juice keytar YOLO occupy knausgaard austin fashion axe lo-fi pickled. Helvetica hammock gentrify church-key forage, keytar freegan fingerstache occupy asymmetrical put a bird on it kogi drinking vinegar keffiyeh. Master cleanse shoreditch cray freegan, craft beer chartreuse put a bird on it meggings. Before they sold out letterpress banh mi literally, cliche chicharrones shabby chic man braid retro slow-carb salvia. Man braid pabst mustache chillwave migas neutra four loko. Trust fund hoodie lomo pop-up swag."
+//New Mongoose Schema with the users' email, name & an association with the postSchema
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [postSchema]
 });
 
-newPost.save(function(err, post){
+//Mongoose model
+
+var User = mongoose.model("User", userSchema);
+
+//Creates a new user
+var newUser = new User({
+    email: "steff@myemail.com",
+    name: "Steff Farley"
+});
+
+//pushes a post into the newUser's posts' array
+newUser.posts.push({
+    title: "An Associated Post",
+    content: "Thundercats ramps etsy try-hard twee +1 williamsburg, farm-to-table readymade direct trade man bun kombucha fashion axe. Narwhal kitsch trust fund, cliche yuccie green juice keytar YOLO occupy knausgaard austin fashion axe lo-fi pickled. Helvetica hammock gentrify church-key forage, keytar freegan fingerstache occupy asymmetrical put a bird on it kogi drinking vinegar keffiyeh. Master cleanse shoreditch cray freegan, craft beer chartreuse put a bird on it meggings. Before they sold out letterpress banh mi literally, cliche chicharrones shabby chic man braid retro slow-carb salvia. Man braid pabst mustache chillwave migas neutra four loko. Trust fund hoodie lomo pop-up swag."
+})
+
+//Saves the newUser information and then tests whether or not the save was succesful
+
+newUser.save(function(err, user){
   if(err){
       console.log(err);
   } else {
-      console.log(post);
+      console.log(user);
   }
 });
+
+// var newPost = new Post({
+//     title: "Making Associative Data with MongoDB",
+//     content: "Thundercats ramps etsy try-hard twee +1 williamsburg, farm-to-table readymade direct trade man bun kombucha fashion axe. Narwhal kitsch trust fund, cliche yuccie green juice keytar YOLO occupy knausgaard austin fashion axe lo-fi pickled. Helvetica hammock gentrify church-key forage, keytar freegan fingerstache occupy asymmetrical put a bird on it kogi drinking vinegar keffiyeh. Master cleanse shoreditch cray freegan, craft beer chartreuse put a bird on it meggings. Before they sold out letterpress banh mi literally, cliche chicharrones shabby chic man braid retro slow-carb salvia. Man braid pabst mustache chillwave migas neutra four loko. Trust fund hoodie lomo pop-up swag."
+// });
+
+// newPost.save(function(err, post){
+//   if(err){
+//       console.log(err);
+//   } else {
+//       console.log(post);
+//   }
+// });
 
