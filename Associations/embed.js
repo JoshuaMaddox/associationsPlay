a612b558deb192083bee848cea4a8780eff68504
Joshua <joshua@eduku.org>
2016-08-11 08:24:08 +0000

    added mongoose user Schema and Model

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,2 +1,12 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
+
+//New Mongoose Schema with the users' email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String
+});
+
+//Mongoose model
+
+var User = mongoose.model("User", userSchema);
